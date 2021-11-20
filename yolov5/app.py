3320268d46083d92eb0d746054ca424e1c343ab2
Hyeongtae <blacksky5066@gmail.com>
2021-11-21 02:46:53 +0900

    update

--- yolov5/app.py
+++ yolov5/app.py
@@ -277,7 +277,7 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5n.pt', help='model path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'best.pt', help='model path(s)')
     parser.add_argument('--source', type=str, default=0, help='file/dir/URL/glob, 0 for webcam')
     #parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob, 0 for webcam')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
